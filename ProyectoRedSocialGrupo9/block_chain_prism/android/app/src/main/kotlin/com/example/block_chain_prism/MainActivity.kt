402f774e032022085a93781d0e81b8e293a4e800
Willy Corzo Lubo <82851253+willyto3@users.noreply.github.com>
2021-11-26 13:19:05 -0500

    Modificacion Proyecto

---
+++ ProyectoRedSocialGrupo9/block_chain_prism/android/app/src/main/kotlin/com/example/block_chain_prism/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.block_chain_prism
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ ProyectoRedSocialGrupo9/block_chain_prism/lib/bottom_bar.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'screens/camera.dart';
+import 'screens/home.dart';
+import 'screens/search.dart';
+import 'screens/shop.dart';
+import 'screens/user.dart';
+
+class BottomBarScreen extends StatefulWidget {
+  static const routeName = "/BottomBarScreen";
+
+  const BottomBarScreen({Key? key}) : super(key: key);
+  @override
+  _BottomBarScreenState createState() => _BottomBarScreenState();
+}
+
+class _BottomBarScreenState extends State<BottomBarScreen> {
+  final List _pages = [
+    const Home(),
+    const Search(),
+    const Camera(),
+    const Shop(),
+    const User(),
+  ];
+  int _selectedIndex = 0;
+  void _selectedPage(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _pages[_selectedIndex],
+      bottomNavigationBar: BottomAppBar(
+        elevation: 5,
+        shape: const CircularNotchedRectangle(),
+        child: BottomNavigationBar(
+          onTap: _selectedPage,
+          backgroundColor: Theme.of(context).primaryColor,
+          selectedItemColor: Colors.purple,
+          unselectedItemColor: Theme.of(context).textSelectionColor,
+          currentIndex: _selectedIndex,
+          items: const [
+            // Icono de Home
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              tooltip: "Home",
+              label: "Home",
+            ),
+            // Icono de Busqueda
+            BottomNavigationBarItem(
+              icon: Icon(Icons.search),
+              tooltip: "Search",
+              label: "Search",
+            ),
+            // Icono de FeedsScreen
+            BottomNavigationBarItem(
+              icon: Icon(Icons.camera),
+              tooltip: "Camera",
+              label: "Camera",
+            ),
+             // Icono de Compras
+            BottomNavigationBarItem(
+              icon: Icon(Icons.shopping_bag),
+              tooltip: "Shop",
+              label: "Shop",
+            ),
+            // Icono de Perfil
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              tooltip: "User",
+              label: "User",
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
