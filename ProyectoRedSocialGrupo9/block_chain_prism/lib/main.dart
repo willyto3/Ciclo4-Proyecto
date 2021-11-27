a4dca83f74e01441eff83ca5705cbf9c18452b64
Willy Corzo Lubo <82851253+willyto3@users.noreply.github.com>
2021-11-27 10:09:14 -0500

    Aplicado el Tema Oscuro

--- ProyectoRedSocialGrupo9/block_chain_prism/lib/main.dart
+++ ProyectoRedSocialGrupo9/block_chain_prism/lib/main.dart
@@ -1,26 +1,47 @@
+import 'package:block_chain_prism/const/theme_data.dart';
 import 'package:flutter/material.dart';
-
+import 'package:provider/provider.dart';
+import 'provider/dark_theme_provider.dart';
 import 'screens/bottom_bar.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
 
-  // This widget is the root of your application.
+class _MyAppState extends State<MyApp> {
+  DarkThemeProvider themeChangeProvider = DarkThemeProvider();
+
+  void getCurrentAppTheme() async {
+    themeChangeProvider.darkTheme =
+        await themeChangeProvider.darkThemePreferences.getTheme();
+  }
+
+  @override
+  void initState() {
+    getCurrentAppTheme();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.purple,
-        splashColor: Colors.teal,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: const BottomBarScreen(),
-    );
+    return MultiProvider(
+        providers: [
+          ChangeNotifierProvider(create: (_) {
+            return themeChangeProvider;
+          })
+        ],
+        child:
+            Consumer<DarkThemeProvider>(builder: (context, themeData, child) {
+          return MaterialApp(
+            title: 'Flutter Demo',
+            theme: Styles.themeData(themeChangeProvider.darkTheme, context),
+            home: BottomBarScreen(),
+          );
+        }));
   }
 }
-
--- ProyectoRedSocialGrupo9/block_chain_prism/lib/screens/user.dart
+++ ProyectoRedSocialGrupo9/block_chain_prism/lib/screens/user.dart
@@ -1,71 +1,233 @@
+
+
+import 'package:block_chain_prism/const/colors.dart';
+import 'package:block_chain_prism/provider/dark_theme_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:list_tile_switch/list_tile_switch.dart';
+import 'package:provider/provider.dart';
 
 class User extends StatefulWidget {
   const User({Key? key}) : super(key: key);
 
   @override
-  State<User> createState() => _UserState();
+  _UserInfoState createState() => _UserInfoState();
 }
 
-class _UserState extends State<User> {
-  bool _value = false;
+class _UserInfoState extends State<User> {
+  late ScrollController _scrollController;
+  var top = 0.0;
+  @override
+  void initState() {
+    // ignore: todo
+    // TODO: implement initState
+    super.initState();
+    _scrollController =ScrollController();
+    _scrollController.addListener(() {setState(() {
+    });});
+  }
   @override
   Widget build(BuildContext context) {
+    final themeChange = Provider.of<DarkThemeProvider>(context);
     return Scaffold(
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
+      body: Stack(
         children: [
-          userTitle("USER INFORMATION"),
-          const Divider(
-            thickness: 1,
-            color: Colors.grey,
+          CustomScrollView(
+            controller: _scrollController,
+            slivers: <Widget>[
+              SliverAppBar(
+                automaticallyImplyLeading: false,
+                elevation: 4,
+                expandedHeight: 200,
+                pinned: true,
+                flexibleSpace: LayoutBuilder(
+                    builder: (BuildContext context, BoxConstraints constraints) {
+                  top = constraints.biggest.height;
+                  return Container(
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                          colors: [
+                            ColorsConsts.starterColor,
+                            ColorsConsts.endColor,
+                          ],
+                          begin: const FractionalOffset(0.0, 0.0),
+                          end: const FractionalOffset(1.0, 0.0),
+                          stops: const [0.0, 1.0],
+                          tileMode: TileMode.clamp),
+                    ),
+                    child: FlexibleSpaceBar(
+                      collapseMode: CollapseMode.parallax,
+                      centerTitle: true,
+                      title: Row(
+                        //  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          AnimatedOpacity(
+                            duration: Duration(milliseconds: 300),
+                            opacity: top <= 110.0 ? 1.0 : 0,
+                            child: Row(
+                              children: [
+                                SizedBox(
+                                  width: 12,
+                                ),
+                                Container(
+                                  height: kToolbarHeight / 1.8,
+                                  width: kToolbarHeight / 1.8,
+                                  decoration: BoxDecoration(
+                                    boxShadow: [
+                                      BoxShadow(
+                                        color: Colors.white,
+                                        blurRadius: 1.0,
+                                      ),
+                                    ],
+                                    shape: BoxShape.circle,
+                                    image: DecorationImage(
+                                      fit: BoxFit.fill,
+                                      image: NetworkImage(
+                                          'https://cdn1.vectorstock.com/i/thumb-large/62/60/default-avatar-photo-placeholder-profile-image-vector-21666260.jpg'),
+                                    ),
+                                  ),
+                                ),
+                                SizedBox(
+                                  width: 12,
+                                ),
+                                Text(
+                                  // 'top.toString()',
+                                  'Guest',
+                                  style: TextStyle(
+                                      fontSize: 20.0, color: Colors.white),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                      background: Image(
+                        image: NetworkImage(
+                            'https://cdn1.vectorstock.com/i/thumb-large/62/60/default-avatar-photo-placeholder-profile-image-vector-21666260.jpg'),
+                        fit: BoxFit.fill,
+                      ),
+                    ),
+                  );
+                }),
+              ),
+              SliverToBoxAdapter(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                        padding: const EdgeInsets.only(left: 8.0),
+                        child: userTitle('User Information')),
+                    Divider(
+                      thickness: 1,
+                      color: Colors.grey,
+                    ),
+                    userListTile('Email', 'Email sub', 0, context),
+                    userListTile('Email', 'Email sub', 0, context),
+                    userListTile('Email', 'Email sub', 0, context),
+                    userListTile('Email', 'Email sub', 0, context),
+                    userListTile('Email', 'Email sub', 0, context),
+                    userListTile('Phone number', '4555', 0, context),
+                    userListTile('Shipping address', '', 0, context),
+                    userListTile('joined date', 'date', 0, context),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 8.0),
+                      child: userTitle('User settings'),
+                    ),
+                    Divider(
+                      thickness: 1,
+                      color: Colors.grey,
+                    ),
+                    ListTileSwitch(
+                      value: themeChange.darkTheme,
+                      leading: Icon(Icons.camera_alt_outlined),
+                      onChanged: (value) {
+                        setState(() {
+                          themeChange.darkTheme = value;
+                        });
+                      },
+                      visualDensity: VisualDensity.comfortable,
+                      switchType: SwitchType.cupertino,
+                      switchActiveColor: Colors.indigo,
+                      title: Text('Dark theme'),
+                    ),
+                    userListTile('Logout', '', 4, context),
+                  ],
+                ),
+              )
+            ],
           ),
-          userListTile("Email", "subtitle", context),
-          userListTile("Phone Number", "subtitle", context),
-          userListTile("Country", "subtitle", context),
-          userTitle("USER SETTINGS"),
-          const Divider(
-            thickness: 1,
-            color: Colors.grey,
-          ),
-          ListTileSwitch(
-            value: _value,
-            leading: const Icon(Icons.mode_night_outlined),
-            onChanged: (value) {
-              setState(() {
-                _value = value;
-              });
-            },
-            visualDensity: VisualDensity.comfortable,
-            switchType: SwitchType.cupertino,
-            switchActiveColor: Colors.purple,
-            title: const Text('Dark Theme'),
-          ),
+          _buildFab()
         ],
       ),
     );
   }
-}
+ 
 
-Widget userListTile(String title, String subTitle, BuildContext context) {
-  return Material(
-    color: Colors.transparent,
-    child: InkWell(
-      //splashColor: Theme.of(context).splashColor
-      child: ListTile(
-        onTap: () {},
-        title: Text(title),
-        subtitle: Text(subTitle),
-        leading: const Icon(Icons.email),
+ Widget _buildFab() {
+    //starting fab position
+    final double defaultTopMargin = 200.0 - 4.0;
+    //pixels from top where scaling should start
+    final double scaleStart = 160.0;
+    //pixels from top where scaling should end
+    final double scaleEnd = scaleStart / 2;
+
+    double top = defaultTopMargin;
+    double scale = 1.0;
+    if (_scrollController.hasClients) {
+      double offset = _scrollController.offset;
+      top -= offset;
+      if (offset < defaultTopMargin - scaleStart) {
+        //offset small => don't scale down
+        scale = 1.0;
+      } else if (offset < defaultTopMargin - scaleEnd) {
+        //offset between scaleStart and scaleEnd => scale down
+        scale = (defaultTopMargin - scaleEnd - offset) / scaleEnd;
+      } else {
+        //offset passed scaleEnd => hide fab
+        scale = 0.0;
+      }
+    }
+
+return  Positioned(
+      top: top,
+      right: 16.0,
+      child:  Transform(
+        transform:  Matrix4.identity()..scale(scale),
+        alignment: Alignment.center,
+        child:  FloatingActionButton(
+          heroTag: "btn1",
+          onPressed: (){},
+          child:  Icon(Icons.camera_alt_outlined),
+        ),
       ),
-    ),
-  );
-}
+    );
+  }
 
-Widget userTitle(String title) {
-  return Text(
-    title,
-    style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
-  );
+
+  Widget userListTile(
+      String title, String subTitle, int index, BuildContext context) {
+    return Material(
+      color: Colors.transparent,
+      child: InkWell(
+        splashColor: Theme.of(context).splashColor,
+        child: ListTile(
+          onTap: () {},
+          title: Text(title),
+          subtitle: Text(subTitle),
+          leading: Icon(Icons.camera_alt_outlined),
+        ),
+      ),
+    );
+  }
+
+  Widget userTitle(String title) {
+    return Padding(
+      padding: const EdgeInsets.all(14.0),
+      child: Text(
+        title,
+        style: TextStyle(fontWeight: FontWeight.bold, fontSize: 23),
+      ),
+    );
+  }
 }
