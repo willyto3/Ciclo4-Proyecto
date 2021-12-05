341b8dd22706162ae82bed207926a0d1cd89780c
Willy Corzo Lubo <82851253+willyto3@users.noreply.github.com>
2021-12-05 16:37:50 -0500

    Tratando de usar Mockito

---
+++ blockchaimprims/test/mocks/api_repository_mocks.dart
@@ -0,0 +1,6 @@
+import 'package:blockchaimprims/features/profile/domain/repositories/api_repository.dart';
+import 'package:mockito/mockito.dart';
+
+class ApiRepositoryMocks extends Mock implements ApiRepositoryInterface{
+
+}
---
+++ blockchaimprims/test/mocks/local_repository_mocks.dart
@@ -0,0 +1,7 @@
+import 'package:blockchaimprims/features/profile/domain/repositories/local_storage_repository.dart';
+import 'package:mockito/mockito.dart';
+
+class LocalRepositoryMocks extends Mock implements LocalRepositoryInterface {
+
+
+}
